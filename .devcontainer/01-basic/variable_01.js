80a6578d12e979dd68dda60c710a36fc50f623e1
adminPriyansh <163454080+adminPriyansh@users.noreply.github.com>
2024-04-19 20:23:10 +0000

    variables

---
+++ .devcontainer/01-basic/variable_01.js
@@ -0,0 +1,7 @@
+const accountId = 125896
+let accountEmail = "priyanshs06@bestbuy.com"
+var accountPasswrd= "1234"
+accountCity ="jaipur"
+let accountState;
+//accountId = 1245 cannot change the value as it is constants.
+console.table([accountId,accountEmail,accountPasswrd,accountCity,accountState])
