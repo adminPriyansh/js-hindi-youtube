d3ba7e1a4487451ff4e465223c253c1f6c9168d5
adminPriyansh <163454080+adminPriyansh@users.noreply.github.com>
2024-04-19 20:30:34 +0000

    datatypes added

---
+++ .devcontainer/01-basic/dataType_02.js
@@ -0,0 +1,12 @@
+"USE STRICT"
+let age=1
+let empName = "Priyansh"
+let isLoginIn= true
+
+let state = null;
+
+console.log(typeof state); //object 
+console.log(typeof age); //number
+console.log(typeof empName); //String
+console.log(typeof isLoginIn); boolean
+console.log(typeof undefined); undefined
